f039ebe37685176b2202b1defa4fe048f6363631
adityaa <adityaa@student.chalmers.se>
2022-04-06 13:08:16 +0200

    Add mod folder for terrain & vehicle

---
+++ mods/terrain/rroverr.gd
@@ -0,0 +1,17 @@
+extends Reference
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+var mod_name: String = "example"
+
+# Called when the node enters the scene tree for the first time.
+func init(global) -> void:
+	global.register_environment("example/Example", preload("res://RestaurantSpicySalmon.tscn"))
+	global.register_vehicle("PPMobile", preload("res://vehicle/PPMobile.tscn"))
+	print("Hello World!")
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
