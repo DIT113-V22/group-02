547d5ec088a202592aeafc9e54c9ef32f64a8d50
adityaa <adityaa@student.chalmers.se>
2022-04-07 17:23:12 +0200

    Upload improved realistic car-like vehicle mod #15
    The new uploaded car is a more real-life replication of a sedan, and is now able to follow the physics of the SMCE default car, and is able to correctly move and turn in the terrain, with sensors working and intact. The sensor's locations also had to be slightly adjusted, as the new vehicle mod has different measurements.

--- mods/terrain/rroverr.gd
+++ mods/terrain/rroverr.gd
@@ -9,7 +9,7 @@ var mod_name: String = "example"
 # Called when the node enters the scene tree for the first time.
 func init(global) -> void:
 	global.register_environment("example/Example", preload("res://RestaurantSpicySalmon.tscn"))
-	global.register_vehicle("PPMobile", preload("res://vehicle/PPMobile.tscn"))
+	global.register_vehicle("TemplateCar", preload("res://TemplateCar.tscn"))
 	print("Hello World!")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
