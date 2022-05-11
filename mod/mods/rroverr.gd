5a8150ad0276ae4435e52e315fcf44a8d311c667
ibrah <gusalgaah@student.gu.se>
2022-05-11 14:15:36 +0200

    Make lanes equal in parking lot #84

--- mod/mods/rroverr.gd
+++ mod/mods/rroverr.gd
@@ -1,4 +1,4 @@
-extends Node
+extends Reference
 
 # Declare member variables here. Examples:
 # var a = 2
@@ -10,14 +10,6 @@ func init(global) -> void:
 	global.register_environment("parking/Lot", preload("res://mods/PocketParkingTerrain.tscn"))
 	global.register_vehicle("PocketParkingVehicle", preload("res://vehicle/PocketParkingVehicle.tscn"))
 	print("Hello World!")
-
-func init_cam_pos() -> Transform:
-	return $CamPosition.global_transform
-
-func get_spawn_position(hint: String) -> Transform:
-	match hint:
-		"debug_vehicle": return $CamPosition.global_transform
-		_: return $VehicleSpawn.global_transform
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
