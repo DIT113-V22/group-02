f5c699ec4942747f516061f4fb5f707b4f5c16ab
Umar Mahmood <gusmahum@student.gu.se>
2022-05-25 16:42:22 +0200

    Fix rounded corners for speedomter and obsDistance textViews #66

--- SmartCarApplication/app/src/main/java/com/example/smartcarapplication/MainActivity.java
+++ SmartCarApplication/app/src/main/java/com/example/smartcarapplication/MainActivity.java
@@ -1,21 +1,16 @@
 package com.example.smartcarapplication;
 
-import android.content.Context;
-import android.content.Intent;
-import android.content.SharedPreferences;
-import android.graphics.BitmapFactory;
 import android.graphics.Bitmap;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
-import android.widget.Button;
 import android.widget.CompoundButton;
 import android.widget.ImageView;
 import android.widget.NumberPicker;
-import android.widget.Toast;
 import android.widget.TextView;
+import android.widget.Toast;
 import android.widget.ToggleButton;
 
 import androidx.appcompat.app.AppCompatActivity;
